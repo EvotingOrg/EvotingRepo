d3be4bf1c8e5fb82dfed47087f3cc83f34e9dcc3
Raj <Raj@172.17.0.41>
2015-02-15 23:13:01 -0600

    ui changes

--- Evoting/src/main/java/com/evoting/controller/UserController.java
+++ Evoting/src/main/java/com/evoting/controller/UserController.java
@@ -14,58 +14,58 @@ import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.EJBException;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
-import javax.faces.bean.ViewScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
 @Named("userController")
-@ViewScoped
+@SessionScoped
 public class UserController extends AbstractViewController<Users> {
-
+    
     @EJB
     private com.evoting.facade.UsersFacade ejbFacade;
     private List<Users> items = null;
-
+    
     public UserController() {
     }
-
+    
     @PostConstruct
     public void init() {
         setSelected(new Users());
         getSelected().setUserDetail(new UserDetail(new Address()));
     }
-
+    
     protected void setEmbeddableKeys() {
     }
-
+    
     protected void initializeEmbeddableKey() {
     }
-
+    
     @Override
     public UsersFacade getFacade() {
         return ejbFacade;
     }
-
+    
     public Users prepareCreate() {
         setSelected(new Users());
         initializeEmbeddableKey();
         return getSelected();
     }
-
+    
     public void create() {
         persist(PersistAction.CREATE, ResourceBundle.getBundle("/Bundle").getString("UsersCreated"));
         if (!JsfUtil.isValidationFailed()) {
             items = null;    // Invalidate list of items to trigger re-query.
         }
     }
-
+    
     public void update() {
         persist(PersistAction.UPDATE, ResourceBundle.getBundle("/Bundle").getString("UsersUpdated"));
     }
-
+    
     public void destroy() {
         persist(PersistAction.DELETE, ResourceBundle.getBundle("/Bundle").getString("UsersDeleted"));
         if (!JsfUtil.isValidationFailed()) {
@@ -73,14 +73,14 @@ public class UserController extends AbstractViewController<Users> {
             items = null;    // Invalidate list of items to trigger re-query.
         }
     }
-
+    
     public List<Users> getItems() {
         if (items == null) {
             items = getFacade().findAll();
         }
         return items;
     }
-
+    
     private void persist(PersistAction persistAction, String successMessage) {
         if (getSelected() != null) {
             setEmbeddableKeys();
@@ -108,44 +108,44 @@ public class UserController extends AbstractViewController<Users> {
             }
         }
     }
-
+    
     public Users getUsers(java.lang.Long id) {
         return getFacade().find(id);
     }
-
+    
     public List<Users> getItemsAvailableSelectMany() {
         return getFacade().findAll();
     }
-
+    
     public List<Users> getItemsAvailableSelectOne() {
         return getFacade().findAll();
     }
-
+    
     @FacesConverter(forClass = Users.class)
     public static class UsersControllerConverter implements Converter {
-
+        
         @Override
         public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
-            if (value == null || value.length() == 0) {
+            if (value == null || value.length() == 0 || value.equals("")) {
                 return null;
             }
             UserController controller = (UserController) facesContext.getApplication().getELResolver().
                     getValue(facesContext.getELContext(), null, "usersController");
             return controller.getUsers(getKey(value));
         }
-
+        
         java.lang.Long getKey(String value) {
             java.lang.Long key;
             key = Long.valueOf(value);
             return key;
         }
-
+        
         String getStringKey(java.lang.Long value) {
             StringBuilder sb = new StringBuilder();
             sb.append(value);
             return sb.toString();
         }
-
+        
         @Override
         public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
             if (object == null) {
@@ -159,7 +159,7 @@ public class UserController extends AbstractViewController<Users> {
                 return null;
             }
         }
-
+        
     }
-
+    
 }
