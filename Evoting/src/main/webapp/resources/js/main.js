2fefc092e24b3b5ff27339526aee145784578fd5
Raj <Raj@Raj-PC>
2015-02-18 01:32:26 -0600

    sign up finalised.

--- Evoting/src/main/webapp/resources/js/main.js
+++ Evoting/src/main/webapp/resources/js/main.js
@@ -1,106 +1,107 @@
-jQuery(function($) {'use strict',
+jQuery(function ($) {
+    'use strict',
+            //#main-slider
+            $(function () {
+                $('#main-slider.carousel').carousel({
+                    interval: 8000
+                });
+            });
 
-	//#main-slider
-	$(function(){
-		$('#main-slider.carousel').carousel({
-			interval: 8000
-		});
-	});
 
-
-	// accordian
-	$('.accordion-toggle').on('click', function(){
-		$(this).closest('.panel-group').children().each(function(){
-		$(this).find('>.panel-heading').removeClass('active');
-		 });
-
-	 	$(this).closest('.panel-heading').toggleClass('active');
-	});
-
-	//Initiat WOW JS
-	new WOW().init();
-
-	// portfolio filter
-	$(window).load(function(){'use strict';
-		var $portfolio_selectors = $('.portfolio-filter >li>a');
-		var $portfolio = $('.portfolio-items');
-		$portfolio.isotope({
-			itemSelector : '.portfolio-item',
-			layoutMode : 'fitRows'
-		});
-		
-		$portfolio_selectors.on('click', function(){
-			$portfolio_selectors.removeClass('active');
-			$(this).addClass('active');
-			var selector = $(this).attr('data-filter');
-			$portfolio.isotope({ filter: selector });
-			return false;
-		});
-	});
-
-	// Contact form
-	var form = $('#main-contact-form');
-	form.submit(function(event){
-		event.preventDefault();
-		var form_status = $('<div class="form_status"></div>');
-		$.ajax({
-			url: $(this).attr('action'),
-
-			beforeSend: function(){
-				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
-			}
-		}).done(function(data){
-			form_status.html('<p class="text-success">' + data.message + '</p>').delay(3000).fadeOut();
-		});
-	});
-
-	
-	//goto top
-	$('.gototop').click(function(event) {
-		event.preventDefault();
-		$('html, body').animate({
-			scrollTop: $("body").offset().top
-		}, 500);
-	});	
-
-	//Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});
-        
-        $(".loginpopup").bind("click", function() {
-		$('#login').modal('toggle');
-	});
-	
-        $('#signup-link').bind("click", function() {
-            $('#login').modal('hide');
-            $('#forgot_password').modal('hide');
-            $('#signup').modal('toggle');           
+    // accordian
+    $('.accordion-toggle').on('click', function () {
+        $(this).closest('.panel-group').children().each(function () {
+            $(this).find('>.panel-heading').removeClass('active');
         });
-        
-        $('#login-link').bind("click", function() {            
-            $('#signup').modal('hide');
-            $('#forgot_password').modal('hide');
-            $('#login').modal('toggle');
+
+        $(this).closest('.panel-heading').toggleClass('active');
+    });
+
+    //Initiat WOW JS
+    new WOW().init();
+
+    // portfolio filter
+    $(window).load(function () {
+        'use strict';
+        var $portfolio_selectors = $('.portfolio-filter >li>a');
+        var $portfolio = $('.portfolio-items');
+        $portfolio.isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
         });
-        
-        $('#login-link1').bind("click", function() {            
-            $('#signup').modal('hide');
-            $('#forgot_password').modal('hide');
-            $('#login').modal('toggle');
+
+        $portfolio_selectors.on('click', function () {
+            $portfolio_selectors.removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $portfolio.isotope({filter: selector});
+            return false;
         });
-        
-        $('#fp-link').bind("click", function() {
-            $('#signup').modal('hide');
-            $('#login').modal('hide');
-            $('#forgot_password').modal('toggle');
+    });
+
+    // Contact form
+    var form = $('#main-contact-form');
+    form.submit(function (event) {
+        event.preventDefault();
+        var form_status = $('<div class="form_status"></div>');
+        $.ajax({
+            url: $(this).attr('action'),
+            beforeSend: function () {
+                form.prepend(form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn());
+            }
+        }).done(function (data) {
+            form_status.html('<p class="text-success">' + data.message + '</p>').delay(3000).fadeOut();
         });
-        
-        $('#logout_buffer').bind("click", function() {            
-            $("#btn_logout").trigger("click");
-        });
-        
-        $('#btn_logout').bind("click", function() {
-            alert('clicked');
-        });
+    });
+
+
+    //goto top
+    $('.gototop').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("body").offset().top
+        }, 500);
+    });
+
+    //Pretty Photo
+    $("a[rel^='prettyPhoto']").prettyPhoto({
+        social_tools: false
+    });
+
+    $(".loginpopup").bind("click", function () {
+        $('#login').modal('toggle');
+    });
+
+    $('.signup-link_popup').bind("click", function () {
+        alert("alert");
+        $('#login').modal('hide');
+        $('#forgot_password').modal('hide');
+        $('#signup').modal('toggle');
+    });
+
+    $('#login-link').bind("click", function () {
+        $('#signup').modal('hide');
+        $('#forgot_password').modal('hide');
+        $('#login').modal('toggle');
+    });
+
+    $('#login-link1').bind("click", function () {
+        $('#signup').modal('hide');
+        $('#forgot_password').modal('hide');
+        $('#login').modal('toggle');
+    });
+
+    $('#fp-link').bind("click", function () {
+        $('#signup').modal('hide');
+        $('#login').modal('hide');
+        $('#forgot_password').modal('toggle');
+    });
+
+    $('#logout_buffer').bind("click", function () {
+        $("#btn_logout").trigger("click");
+    });
+
+    $('#btn_logout').bind("click", function () {
+        alert('clicked');
+    });
 });
